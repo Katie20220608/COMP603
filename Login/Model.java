408a53287024846900709ed9a3150a5b2c7df51d
Yue LI <yueli.7888@gmail.com>
2022-10-16 21:33:57 +1300

    firstGUIWindow

---
+++ Login/Model.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Login;
+
+import java.util.Observable;
+
+/**
+ *
+ * @author yueli
+ */
+public class Model extends Observable{
+    
+    
+    
+}
